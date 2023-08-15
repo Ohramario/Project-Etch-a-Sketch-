fa691ad22ad7e75d09daa90ac4588c258b2a48dc
Kris <93468625+Ohramario@users.noreply.github.com>
2023-08-15 14:28:12 +0300

    added 16 horizontally-aligned boxes within each verticall-aligned boxes

--- JS/grid.js
+++ JS/grid.js
@@ -1,20 +1,33 @@
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
 
 
-function column() {
+function createGrid() {
 
 
     for (let i = 0; i < 16; i++) {
 
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = 'BOX';
-    
-        container.appendChild(content);
-      } 
+        const column = document.createElement("div");
+        column.classList.add("column");
+        container.appendChild(column);
 
-  
+
+
+
+        for (let i = 0; i < 16; i++) {
+
+            let row = document.createElement("div");
+            row.classList.add("row");
+            column.appendChild(row);
+
+        }
+
+
+
+
+    }
+
+
 
 }
 
-column();
+createGrid();
